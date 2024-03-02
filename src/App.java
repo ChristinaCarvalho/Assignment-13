e63075331f475113a758f0aa8259578caa66c19b
Christina Carvalho <ccarvalho@oakland.edu>
2024-03-01 23:04:16 -0500

    init

--- src/App.java
+++ src/App.java
@@ -1,10 +1,7 @@
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class App {
